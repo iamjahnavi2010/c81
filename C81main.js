8a7ef1eb4baaadf1751f22881d18e42d12768491
iamjahnavi2010 <78913699+iamjahnavi2010@users.noreply.github.com>
2021-07-12 15:43:10 +0530

    Update C81main.js

--- C81main.js
+++ C81main.js
@@ -27,9 +27,4 @@ function cleararea()
 {
     ctx.clearRect(0,0,canvas.width,canvas.height);
 }
-color ="blue";
-ctx.beginPath();
-ctx.strokeStyle=color;
-ctx.lineWidth=2
-ctx.arc(200,200,40,0,2*Math.PI);
-ctx.stroke();
+
